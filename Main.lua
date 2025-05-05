5fa0e843352d9480e1a6d6d6f2fc89c12f0e3d3e
plusmouse <plusmouse@protonmail.com>
2025-05-05 16:37:24 +0100

    Fix for latest PBS changes

--- Main.lua
+++ Main.lua
@@ -1,10 +1,12 @@
 local realCompose = Auctionator.Search.ComposeTooltip
 local LibSerialize = LibStub("LibSerialize")
 
+local source = {}
+
 Auctionator.Search.ComposeTooltip = function(searchTerm)
-  if PointBlankSniper.ItemKeyCache.State.orderedKeys == nil then
-    PointBlankSniper.ItemKeyCache.State.orderedKeys = select(2, LibSerialize:Deserialize(POINT_BLANK_SNIPER_ITEM_CACHE.orderedKeys))
-  end
+  Auctionator.EventBus:RegisterSource(source, "compose tooltip")
+      :Fire(source, PointBlankSniper.Events.SetupKeysSearch)
+      :UnregisterSource(source)
   local result = realCompose(searchTerm)
   local list = CreateAndInitFromMixin(AuctionatorShoppingListMixin, {items = {searchTerm}}, {})
   local amount = #PointBlankSniper.Scan.GetItemKeys(PointBlankSniper.Utilities.ConvertList(list))
